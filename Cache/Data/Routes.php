6a3ff02720469fd638539a8433bc229ee5a9b553
李文岭 <lwl_0321@126.com>
2017-10-16 11:59:24 +0800

    10-16-1

--- Cache/Data/Routes.php
+++ Cache/Data/Routes.php
@@ -12,14 +12,14 @@ return array(
 'Tags/:p\d' => 'Home/Tags/index',
 'Tags/:tag' => 'Home/Tags/index',
 'Tags' => 'Home/Tags/index',
-'/^(cn|en)$/' => 'Index/index?l=:1',
-'/^(cn|en)\/([A-Z]{1}[a-z]+)\/show\/(\d+)-(\d+)$/' => 'Urlrule/show?l=:1&module=:2&id=:3&p=:4&',
+'/^(fc|cn|en)$/' => 'Index/index?l=:1',
+'/^(fc|cn|en)\/([A-Z]{1}[a-z]+)\/show\/(\d+)-(\d+)$/' => 'Urlrule/show?l=:1&module=:2&id=:3&p=:4&',
 '/^([A-Z]{1}[a-z]+)\/show\/(\d+)-(\d+)$/' => 'Urlrule/show?module=:1&id=:2&p=:3&',
-'/^(cn|en)\/([A-Z]{1}[a-z]+)\/show\/(\d+)$/' => 'Urlrule/show?l=:1&module=:2&id=:3&',
+'/^(fc|cn|en)\/([A-Z]{1}[a-z]+)\/show\/(\d+)$/' => 'Urlrule/show?l=:1&module=:2&id=:3&',
 '/^([A-Z]{1}[a-z]+)\/show\/(\d+)$/' => 'Urlrule/show?module=:1&id=:2&',
-'/^(cn|en)\/([A-Z]{1}[a-z]+)\/list\/(\d+)-(\d+)$/' => 'Urlrule/index?l=:1&module=:2&catid=:3&p=:4&',
+'/^(fc|cn|en)\/([A-Z]{1}[a-z]+)\/list\/(\d+)-(\d+)$/' => 'Urlrule/index?l=:1&module=:2&catid=:3&p=:4&',
 '/^([A-Z]{1}[a-z]+)\/list\/(\d+)-(\d+)$/' => 'Urlrule/index?module=:1&catid=:2&p=:3&',
-'/^(cn|en)\/([A-Z]{1}[a-z]+)\/list\/(\d+)$/' => 'Urlrule/index?l=:1&module=:2&catid=:3&',
+'/^(fc|cn|en)\/([A-Z]{1}[a-z]+)\/list\/(\d+)$/' => 'Urlrule/index?l=:1&module=:2&catid=:3&',
 '/^([A-Z]{1}[a-z]+)\/list\/(\d+)$/' => 'Urlrule/index?module=:1&catid=:2&'
 );
 ?>
